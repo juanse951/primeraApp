0e4c2e177ff2f0b9b066e1a41405bfd49887c2bb
juanse951 <juanse951@gmail.com>
2024-09-14 09:09:29 -0500

    Creacion de seccion decisiones e interectividad con usuario en seccion lectura

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaDeLanzamiento = 1999;
+        boolean incluidoEnElPlan = true ;
+        double notaDeLaPelicula = 8.2 ;
+        String tipoPlan = "plus";
+
+        if(fechaDeLanzamiento >= 2022){
+            System.out.println("Péliculas más populares");
+        }else {
+            System.out.println("Péliculas Retro que aún vale la pena ver");
+        }
+
+        if (incluidoEnElPlan && tipoPlan.equals("plus")){
+            System.out.println("Disfrute de su película");
+        } else{
+            System.out.println("Película no disponible para su plan actual");
+        }
+    }
 }
